aac88a687693683943fed4a59fbdb1c4424d831b
Artun Kara <artunankara@gmail.com>
2022-08-26 10:36:32 +0300

    dice update

--- Dicee-Game/index.js
+++ Dicee-Game/index.js
@@ -2,9 +2,9 @@ var randomNumber1;
 var randomNumber2;
 
 randomNumber1 = Math.floor((Math.random() * 6) + 1);
-document.querySelectorAll("img")[0].setAttribute("src", "images\\dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[0].setAttribute("src", `images\\dice${randomNumber1}.png`);
 randomNumber2 = Math.floor((Math.random() * 6) + 1);
-document.querySelectorAll("img")[1].setAttribute("src", "images\\dice" + randomNumber2 + ".png")
+document.querySelectorAll("img")[1].setAttribute("src", `images\\dice${randomNumber2}.png`);
 if(randomNumber1 < randomNumber2)
 {
     document.querySelector("h1").innerHTML = ("Player 2 Wins");
